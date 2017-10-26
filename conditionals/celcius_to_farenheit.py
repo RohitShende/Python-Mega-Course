5a375172ce7f8ceb6686e2d8f7b8a122404714c7
Rohit Shende <rohitshende16@gmail.com>
2017-10-27 00:41:20 +0530

    Adding conditional module

---
+++ conditionals/celcius_to_farenheit.py
@@ -0,0 +1,5 @@
+def celcius_to_farenheit(temp_in_celcius):
+    return temp_in_celcius*(9/5) + 32
+
+
+print(celcius_to_farenheit(int(input('Enter Temperature in Celcius:'))))
---
+++ conditionals/sample.py
@@ -0,0 +1,6 @@
+def age_foo():
+    return age + 40
+
+
+age = int(input('Enter your Age :'))
+print(age_foo())
